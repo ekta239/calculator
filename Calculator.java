af346fcc8fb97acb7c8f9aa3865548e503d9fbe2
Ekta verma <ektav8231@gmail.com>
2025-01-07 22:56:04 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Calculator {
+public static void main(String[] args) {
+Scanner sc= new Scanner(System.in);
+System.out.println("####Calculator####");
+System.out.println("u can perform + - * / ");
+System.out.print("Please Enter the opration:");
+char o=sc.next().charAt(0);
+System.out.print("Enter the first number: ");
+int num1=sc.nextInt();
+System.out.print("Enter the second number: ");
+int num2= sc.nextInt();
+}
+}
